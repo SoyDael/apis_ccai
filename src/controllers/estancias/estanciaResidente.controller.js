3d66c8d6669dced6f7139251f35dfafc840b0d39
SoyDael <guhs202011004@tese.edu.mx>
2024-06-04 20:52:07 -0600

    editar perfil
    se agrega el api de editar perfil en estancias

--- src/controllers/estancias/estanciaResidente.controller.js
+++ src/controllers/estancias/estanciaResidente.controller.js
@@ -39,3 +39,28 @@ export const consultaEstanciaResidente = async (req, res) => {
     }
 }
 
+export const actualizarPerfilEstancia = async (req, res) => {
+    const {correo} = req.params;
+    const {id_estancia_residente, nombres, apellido_p, apellido_m, correo_adicional, telefono, estatus} = req.body;
+    try {
+        const [result] = await pool.query("UPDATE estancia_residente SET nombres = IFNULL(?, nombres), apellido_p = IFNULL(?, apellido_p), apellido_m = IFNULL(?, apellido_m), correo_adicional = IFNULL(?, correo_adicional), telefono = IFNULL(?, telefono), estatus = IFNULL(?, estatus) WHERE correo = ?", [nombres, apellido_p, apellido_m, correo_adicional, telefono, estatus, correo]);
+    
+        if (result.affectedRows === 0) {
+            res.status(404).json({
+                message: "No se encontró el perfil de la estancia"
+            });
+            return;
+        }
+
+        const [rows] = await pool.query("SELECT * FROM estancia_residente WHERE correo = ?", [correo]);
+        res.send(rows);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "¡Algo salió mal UwU!",
+            error,
+            
+        });
+    }
+}
--- src/routes/Estancias/estanciasResidente.routes.js
+++ src/routes/Estancias/estanciasResidente.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../../controllers/auth.controller.js";
-import { registroEstanciaResidente, consultaEstanciaResidente } from "../../controllers/estancias/estanciaResidente.controller.js";
+import { registroEstanciaResidente, consultaEstanciaResidente, actualizarPerfilEstancia } from "../../controllers/estancias/estanciaResidente.controller.js";
 
 const router = Router();
 
 router.post('/registroEstanciaResidente', verifyToken, registroEstanciaResidente);
 router.get('/consultaEstanciaResidente', verifyToken, consultaEstanciaResidente);
+router.patch('/actualizarPerfilEstancia/:correo', verifyToken, actualizarPerfilEstancia);
 
 export default router;
