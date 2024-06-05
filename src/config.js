f22d11547afc1997161adbcf1084d1b0190a156a
JohanHG1015 <jonn101544@gmail.com>
2024-06-05 11:19:58 -0600

    root

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ config();
 
 export const PORT = process.env.PORT || 3300;
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Password1';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
 export const DB_NAME = process.env.DB_NAME || 'ccai_api_v1';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || 3306;
